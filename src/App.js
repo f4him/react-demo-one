4fd674fc7e4cc2ee0079589215c87ac8da8b366a
f4him <shahriarfahim23@gmail.com>
2023-06-30 02:36:31 +0600

    little tweaks

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/contact.scss"
 import "./styles/mediaquery.scss"
+import "./styles/services.scss"
 
 function App() {
   return (
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import {Carousel} from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from "../assets/1.jpg";
-import img2 from "../assets/2.png";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+import img5 from "../assets/5.jpg";
+import img6 from "../assets/6.jpg";
 const Services = () => {
   return (
     <>
+    <div className="wrapper">
     <div className="services">
-        <Carousel infiniteLoop autoPlay showStatus={false} interval={1000}>
+        <Carousel infiniteLoop autoPlay interval={1000}>
             <div>
-                <img src={img1} alt='random text'/>
-                <p className='legend'>Full Stack</p>
+                <img className='leg' src={img3} alt='random text'/>
+                <p className='legend'>Legend one</p>
             </div>
             <div>
-                <img src={img2} alt='random text'/>
-                <p className='legend'>New job</p>
+                <img className='leg' src={img4} alt='random text'/>
+                <p className='legend'>Legend two</p>
+            </div>
+            <div>
+                <img className='leg' src={img5} alt='random text'/>
+                <p className='legend'>Legend three</p>
+            </div>
+            <div>
+                <img className='leg' src={img6} alt='random text'/>
+                <p className='legend'>Legend four</p>
             </div>
         </Carousel>
     </div>
-    
+    </div>
     </>
     )
 }
